8296beac2eeb50ada56db59705d834a45482f4e4
Ewan Crowle <dev@crowle.me>
2026-03-24 20:49:23 +0000

    Add accountRouter to appRouter in API server

--- apps/api-server/src/trpc/routers/app.ts
+++ apps/api-server/src/trpc/routers/app.ts
@@ -1,8 +1,10 @@
 import { router } from "../index";
 import { authRouter } from "./auth";
+import {accountRouter} from "./account";
 
 export const appRouter = router({
   auth: authRouter,
+  account: accountRouter,
 });
 
 export type AppRouter = typeof appRouter;
