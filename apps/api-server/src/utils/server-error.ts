7ac8db0fc0418d0b35b82fcab7de030d1be87469
Ewan Crowle <dev@crowle.me>
2026-03-24 14:27:55 +0000

    Improve the server-error.ts utility

--- apps/api-server/src/utils/server-error.ts
+++ apps/api-server/src/utils/server-error.ts
@@ -2,27 +2,51 @@ import { Code, ConnectError } from "@connectrpc/connect";
 import { TRPCError } from "@trpc/server";
 
 export default function (error: TRPCError) {
+  if (!error.cause || !(error.cause instanceof Error)) {
+    return;
+  }
+
   const connectErr = ConnectError.from(error.cause);
+
   switch (connectErr.code) {
     case Code.AlreadyExists:
       throw new TRPCError({
         code: "CONFLICT",
         message: connectErr.message,
+        cause: error.cause,
       });
     case Code.InvalidArgument:
       throw new TRPCError({
         code: "BAD_REQUEST",
         message: connectErr.message,
+        cause: error.cause,
       });
     case Code.NotFound:
       throw new TRPCError({
         code: "NOT_FOUND",
         message: connectErr.message,
+        cause: error.cause,
       });
-    default:
+    case Code.Unauthenticated:
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: connectErr.message,
+        cause: error.cause,
+      });
+    case Code.PermissionDenied:
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: connectErr.message,
+        cause: error.cause,
+      });
+    case Code.Internal:
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
         message: connectErr.message,
+        cause: error.cause,
       });
+    default:
+      return;
   }
 }
+
