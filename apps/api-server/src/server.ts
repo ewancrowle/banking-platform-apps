c404acca84e4f28f28499e65ee932fde4854c836
Ewan Crowle <dev@crowle.me>
2026-03-24 17:59:27 +0000

    Resolve symbol resolution error in server.ts

--- apps/api-server/src/server.ts
+++ apps/api-server/src/server.ts
@@ -1,13 +1,14 @@
-import { createHTTPServer } from "@trpc/server/adapters/standalone";
-import { appRouter } from "./trpc/routers/app";
-import { createContext } from "./utils/context";
+import {createHTTPServer} from "@trpc/server/adapters/standalone";
+import {appRouter} from "./trpc/routers/app";
+import {createContext} from "./utils/context";
 import serverError from "./utils/server-error";
 
 const server = createHTTPServer({
   router: appRouter,
   createContext,
   onError: (opts) => {
-    console.error("Error:", error);
+    const {cause} = opts.error;
+    if (cause) console.error(cause);
     serverError(opts.error)
   },
 });
