366a1bb3cb58c7ffc216278d5f1520b7daf516f7
Ewan Crowle <dev@crowle.me>
2026-04-12 15:08:18 +0100

    Rename protos/ to ts-protos/

--- apps/api-server/src/clients/account.ts
+++ apps/api-server/src/clients/account.ts
@@ -4,8 +4,8 @@ import { AccountService } from "protos/account";
 import { env } from "../utils/env";
 
 const transport = createConnectTransport({
-  httpVersion: "2",
-  baseUrl: env.ACCOUNT_SERVICE_URL,
+	httpVersion: "2",
+	baseUrl: env.ACCOUNT_SERVICE_URL,
 });
 
 const client = createClient(AccountService, transport);
