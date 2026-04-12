f4ab7ca27ca9cdb99d3bf51f10c7d295bd319329
Ewan Crowle <dev@crowle.me>
2026-04-12 14:49:12 +0100

    Implement request rate limiting using @trpc-limiter/memory

--- apps/api-server/src/trpc/index.ts
+++ apps/api-server/src/trpc/index.ts
@@ -1,24 +1,35 @@
 import { initTRPC, TRPCError } from "@trpc/server";
-import { Context } from "../utils/context";
+import { createTRPCStoreLimiter } from "@trpc-limiter/memory";
 import superjson from "superjson";
+import type { Context } from "../utils/context";
 
 const t = initTRPC.context<Context>().create({
-  transformer: superjson,
+	transformer: superjson,
 });
 
 export const router = t.router;
 
-export const publicProcedure = t.procedure;
-
-export const protectedProcedure = t.procedure.use(async (opts) => {
-  const { ctx } = opts;
-  if (!ctx.account) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-
-  return opts.next({
-    ctx: {
-      account: ctx.account,
-    },
-  });
+const rateLimiter = createTRPCStoreLimiter<typeof t>({
+	fingerprint: (ctx) => ctx.ipAddress,
+	message: (retryAfter) =>
+		`Too many requests. Slow down. Retry in ${retryAfter}s.`,
+	max: 15,
+	windowMs: 10000,
 });
+
+export const publicProcedure = t.procedure.use(rateLimiter);
+
+export const protectedProcedure = t.procedure
+	.use(rateLimiter)
+	.use(async (opts) => {
+		const { ctx } = opts;
+		if (!ctx.account) {
+			throw new TRPCError({ code: "UNAUTHORIZED" });
+		}
+
+		return opts.next({
+			ctx: {
+				account: ctx.account,
+			},
+		});
+	});
--- apps/api-server/src/utils/context.ts
+++ apps/api-server/src/utils/context.ts
@@ -1,34 +1,45 @@
 import type { CreateHTTPContextOptions } from "@trpc/server/adapters/standalone";
+import type { IncomingMessage } from "http";
 import accountService from "../clients/account";
 import oauthService from "../clients/oauth";
 
+export const ipAddress = (req: IncomingMessage) => {
+	const forwarded = req.headers["x-forwarded-for"];
+	const ip = forwarded
+		? (typeof forwarded === "string" ? forwarded : forwarded[0])?.split(/, /)[0]
+		: req.socket.remoteAddress;
+	return ip || "127.0.0.1";
+};
+
 export async function createContext({ req }: CreateHTTPContextOptions) {
-  if (!req.headers.authorization) {
-    return {
-      account: null,
-      ipAddress: req.socket.remoteAddress,
-      userAgent: req.headers["user-agent"],
-    };
-  }
+	if (!req.headers.authorization) {
+		return {
+			account: null,
+			ipAddress: ipAddress(req),
+			userAgent: req.headers["user-agent"],
+		};
+	}
 
-  try {
-    const { accountId } = await oauthService.introspect({
-      accessToken: req.headers.authorization.split(" ")[1],
-    });
+	try {
+		const { accountId } = await oauthService.introspect({
+			accessToken: req.headers.authorization.split(" ")[1],
+		});
 
-    const account = await accountService.getAccount({
-      id: accountId,
-    });
+		const account = await accountService.getAccount({
+			id: accountId,
+		});
 
-    return {
-      account,
-    };
-  } catch {
-    return {
-      account: null,
-      ipAddress: req.socket.remoteAddress,
-      userAgent: req.headers["user-agent"],
-    };
-  }
+		return {
+			account,
+			ipAddress: ipAddress(req),
+			userAgent: req.headers["user-agent"],
+		};
+	} catch {
+		return {
+			account: null,
+			ipAddress: ipAddress(req),
+			userAgent: req.headers["user-agent"],
+		};
+	}
 }
 export type Context = Awaited<ReturnType<typeof createContext>>;
